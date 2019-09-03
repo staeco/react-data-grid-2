1b74f7d7482d3979e065f0af9f7ca668eea8f435
Steve Lacy <me@slacy.me>
2019-09-03 10:23:13 -0700

    lint

--- example/App.js
+++ example/App.js
@@ -1,53 +1,65 @@
-import React, { PureComponent } from 'react'
-import Grid from '../lib'
-import '../dist/react-data-grid.css'
-import './app.sass'
+import React, { PureComponent } from "react"
+import Grid from "../lib"
+import "../dist/react-data-grid.css"
+import "./app.sass"
 
 const columns = [
   { key: "id", name: "ID", sortable: true },
   { key: "title", name: "Title", sortable: true },
-  { key: "complete", name: "Complete", sortable: true }
+  { key: "complete", name: "Complete", sortable: true },
 ]
 
 const rows = [
   { id: 0, title: "Task 1", complete: 20 },
   { id: 1, title: "Task 2", complete: 100 },
-  { id: 2, title: "Task 3", complete: 60 }
+  { id: 2, title: "Task 3", complete: 60 },
 ]
 
-const sortRows = (initialRows, sortColumn, sortDirection) => rows => {
+const sortRows = (initialRows, sortColumn, sortDirection) => (rows) => {
   const comparer = (a, b) => {
     if (sortDirection === "ASC") {
-      return a[sortColumn] > b[sortColumn] ? 1 : -1;
+      return a[sortColumn] > b[sortColumn] ? 1 : -1
     } else if (sortDirection === "DESC") {
-      return a[sortColumn] < b[sortColumn] ? 1 : -1;
+      return a[sortColumn] < b[sortColumn] ? 1 : -1
     }
-  };
-  return sortDirection === "NONE" ? initialRows : [...rows].sort(comparer);
+  }
+  return sortDirection === "NONE" ? initialRows : [...rows].sort(comparer)
 }
 
 export default class App extends PureComponent {
   state = {
-    sortColumn: 'id',
-    sortDirection: 'ASC',
+    sortColumn: "id",
+    sortDirection: "ASC",
     rows,
   }
   onGridSort = (sortColumn, sortDirection) => {
-    const updated = sortRows(rows, sortColumn, sortDirection)(this.state.rows)
-    this.setState({ sortColumn, sortDirection, rows: updated })
+    this.setState({ loading: true }, () => {
+      setTimeout(() => {
+        const updated = sortRows(rows, sortColumn, sortDirection)(
+          this.state.rows
+        )
+        this.setState({
+          sortColumn,
+          sortDirection,
+          rows: updated,
+          loading: false,
+        })
+      }, 2000)
+    })
   }
   render = () => {
     const rowGetter = (i) => this.state.rows[i]
-    return <div className="app">
-      <Grid
-        columns={columns}
-        rowGetter={rowGetter}
-        rowsCount={this.state.rows.length}
-        onGridSort={this.onGridSort}
-        sortColumn={this.state.sortColumn}
-        sortDirection={this.state.sortDirection}
-      />
-    </div>
-
+    return (
+      <div className="app">
+        <Grid
+          columns={columns}
+          rowGetter={rowGetter}
+          rowsCount={this.state.rows.length}
+          onGridSort={this.onGridSort}
+          sortColumn={this.state.sortColumn}
+          sortDirection={this.state.sortDirection}
+        />
+      </div>
+    )
   }
 }
